fdae24ff12eefe917d04812694266ea356107dcd
CryptoN999 <nicholasgallucci4@gmail.com>
2019-10-18 20:48:06 -0700

    barebones nav

--- my-tipease/src/App.js
+++ my-tipease/src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { UserSignup } from "./Components/User/UserSignup";
 import './App.css';
 import WorkerDetail from './Components/WorkersDetail';
-
+// import { Route } from "react-router-dom";
+import { Nav } from './Components/NavLink';
 
 
 function App() {
   return (
     <div className="App">
       <UserSignup />
+      <Nav />
+      {/* <Route path="" component={}></Route>
+      <Route path="" component={}></Route>
+      <Route path="" component={}></Route>
+      <Route path="" component={}></Route>
+      <Route path="" component={}></Route> */}
     </div>
   );
 }
