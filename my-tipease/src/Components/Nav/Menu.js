39e2c4081f97707161f017f5b28c7dde409cfff6
CryptoN999 <nicholasgallucci4@gmail.com>
2019-10-22 18:58:30 -0700

    took away nav text decoration

--- my-tipease/src/Components/Nav/Menu.js
+++ my-tipease/src/Components/Nav/Menu.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export default ({ close }) => (
   <div className="menu">
     <ul>
-      <li onClick={close}><Link to="/UserLogin">Home</Link></li>
-      <li onClick={close}><Link to="/WorkersList">WorkersList</Link></li>
-      <li onClick={close}><Link to="/UserSignup">Sign Up</Link></li>
+      <li onClick={close}><Link to="/UserLogin" style={{ textDecoration: 'none' color: 'red' }}>Home</Link></li>
+      <li onClick={close}><Link to="/WorkersList" style={{ textDecoration: 'none' }}>WorkersList</Link></li>
+      <li onClick={close}><Link to="/UserSignup" style={{ textDecoration: 'none' }}>Sign Up</Link></li>
     </ul>
   </div>
 );
