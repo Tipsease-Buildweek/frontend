f36c99c742433dfe83d8103d7b6aff3e9c907c0e
CryptoN999 <nicholasgallucci4@gmail.com>
2019-10-22 19:30:33 -0700

    fixed hamburger menu

--- my-tipease/src/Components/Nav/Menu.js
+++ my-tipease/src/Components/Nav/Menu.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
+
 export default ({ close }) => (
   <div className="menu">
     <ul>
-      <li onClick={close}><Link to="/UserLogin" style={{ textDecoration: 'none' color: 'red' }}>Home</Link></li>
+      <li onClick={close}><Link to="/UserLogin" style={{ textDecoration: 'none' }}>Home</Link></li>
       <li onClick={close}><Link to="/WorkersList" style={{ textDecoration: 'none' }}>WorkersList</Link></li>
       <li onClick={close}><Link to="/UserSignup" style={{ textDecoration: 'none' }}>Sign Up</Link></li>
     </ul>
