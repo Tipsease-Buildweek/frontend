58e653d3f76361687ee9e589f9d9ed94bf0e0cb0
Navid Karami <navid.gsz@gmail.com>
2019-10-23 18:50:19 -0700

    fixed routes

--- my-tipease/src/Components/Nav/Menu.js
+++ my-tipease/src/Components/Nav/Menu.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-
 export default ({ close }) => (
-  <div className="menu">
+  <div className='menu'>
     <ul>
-      <li onClick={close}><Link to="/UserLogin" style={{ textDecoration: 'none' }}>Log In</Link></li>
-      <li onClick={close}><Link to="/WorkersList" style={{ textDecoration: 'none' }}>Meet Our Servers!</Link></li>
-      <li onClick={close}><Link to="/UserSignup" style={{ textDecoration: 'none' }}>Sign Up</Link></li>
+      <li onClick={close}>
+        <Link to='/UserLogin' style={{ textDecoration: 'none' }}>
+          Log In
+        </Link>
+      </li>
+      <li onClick={close}>
+        <Link to='/' style={{ textDecoration: 'none' }}>
+          Meet Our Servers!
+        </Link>
+      </li>
+      <li onClick={close}>
+        <Link to='/UserSignup' style={{ textDecoration: 'none' }}>
+          Sign Up
+        </Link>
+      </li>
     </ul>
   </div>
 );
