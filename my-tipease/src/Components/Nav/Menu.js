4e3cf172d6909654184b3c6982b24c72cf91a2c8
CryptoN999 <nicholasgallucci4@gmail.com>
2019-10-23 18:52:48 -0700

    added logout in Nav that removes token

--- my-tipease/src/Components/Nav/Menu.js
+++ my-tipease/src/Components/Nav/Menu.js
@@ -8,6 +8,7 @@ export default ({ close }) => (
       <li onClick={close}><Link to="/UserLogin" style={{ textDecoration: 'none' }}>Log In</Link></li>
       <li onClick={close}><Link to="/WorkersList" style={{ textDecoration: 'none' }}>Meet Our Servers!</Link></li>
       <li onClick={close}><Link to="/UserSignup" style={{ textDecoration: 'none' }}>Sign Up</Link></li>
+      <li onClick={close}><Link to="/UserLogin" onClick={() => {localStorage.removeItem("token");}} style={{ textDecoration: 'none' }}>Log Out!</Link></li>
     </ul>
   </div>
 );
