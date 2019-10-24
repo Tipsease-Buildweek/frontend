25224eeae0ffed2d01d99dc7b9132004ad71ee57
Navid Karami <navid.gsz@gmail.com>
2019-10-23 19:55:12 -0700

    fixed signup redirect

--- my-tipease/src/Components/User/UserSignup.js
+++ my-tipease/src/Components/User/UserSignup.js
@@ -1,144 +1,139 @@
-import React, { useState } from "react";
-import { axiosWithAuth } from "../../Utils/axiosWithAuth";
-import { Link } from "react-router-dom";
-import TipRoutes from "../../Components/Routes";
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../../Utils/axiosWithAuth';
+import { Link } from 'react-router-dom';
+import TipRoutes from '../../Components/Routes';
 import {
-    CardWrapper,
-    CardHeader,
-    CardHeading,
-    CardBody,
-    CardIcon,
-    CardFieldset,
-    CardInput,
-    CardOptionsItem,
-    CardOptions,
-    CardButton,
-    CardLink
-} from "./UserCard";
+  CardWrapper,
+  CardHeader,
+  CardHeading,
+  CardBody,
+  CardIcon,
+  CardFieldset,
+  CardInput,
+  CardOptionsItem,
+  CardOptions,
+  CardButton,
+  CardLink
+} from './UserCard';
 
+export const UserSignup = props => {
+  const [user, setUser] = useState({
+    fullName: '',
+    password: '',
+    photoUrl: '',
+    username: ''
+  });
+  const [isLoading, setIsLoading] = useState(false);
 
-export const UserSignup = (props) => {
-    const [user, setUser] = useState({
-        fullName: "",
-        password: "",
-        photoUrl: "",
-        username: ""
-    })
-    const [isLoading, setIsLoading] = useState(false);
+  const handleChange = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    });
+  };
 
-    const handleChange = e => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    axiosWithAuth()
+      .post('/auth/users/register', user)
+      .then(res => {
+        console.log('Add User:', res);
         setUser({
-            ...user,
-            [e.target.name]: e.target.value
-        })
-    }
+          fullName: '',
+          password: '',
+          photoUrl: '',
+          username: ''
+        });
+        props.history.push('/userlogin');
+        window.location.reload(false);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        setIsLoading(true);
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <CardWrapper>
+          <CardHeader>
+            <CardHeading>Sign Up</CardHeading>
+          </CardHeader>
 
-        axiosWithAuth()
-            .post("/auth/users/register", user)
-            .then(res => {
-                console.log("Add User:", res)
-                setUser({
+          <CardBody>
+            <CardFieldset>
+              <CardInput
+                type='text'
+                placeholder='Full Name'
+                name='fullName'
+                value={user.fullName}
+                onChange={handleChange}
+                required
+              />
+            </CardFieldset>
+            <CardFieldset>
+              <CardInput
+                placeholder='Password'
+                name='password'
+                type='password'
+                value={user.password}
+                onChange={handleChange}
+                required
+              />
+              <CardIcon className='fa fa-eye' eye small />
+            </CardFieldset>
+            <CardFieldset>
+              <CardInput
+                placeholder='Photo Url'
+                name='photoUrl'
+                type='link'
+                value={user.photoUrl}
+                onChange={handleChange}
+                required
+              />
+            </CardFieldset>
 
-                    fullName: "",
-                    password: "",
-                    photoUrl: "",
-                    username: ""
+            <CardFieldset>
+              <CardInput
+                placeholder='Username'
+                name='username'
+                type='text'
+                value={user.username}
+                onChange={handleChange}
+                required
+              />
+            </CardFieldset>
 
-                })
-                props.history.push('/workerslist');
-                window.location.reload(false);
+            <CardFieldset>
+              <CardOptions>
+                <CardOptionsItem>
+                  <CardIcon className='fab fa-google' big />
+                </CardOptionsItem>
 
-            })
-            .catch(err => {
-                console.log(err)
-            });
-    }
+                <CardOptionsItem>
+                  <CardIcon className='fab fa-twitter' big />
+                </CardOptionsItem>
 
+                <CardOptionsItem>
+                  <CardIcon className='fab fa-facebook' big />
+                </CardOptionsItem>
+              </CardOptions>
+            </CardFieldset>
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <CardWrapper>
-                    <CardHeader>
-                        <CardHeading>Sign Up</CardHeading>
-                    </CardHeader>
+            <CardFieldset>
+              <CardButton type='submit'>Sign Up</CardButton>
+            </CardFieldset>
 
-                    <CardBody>
-                        <CardFieldset>
-                            <CardInput
-                                type="text"
-                                placeholder="Full Name"
-                                name="fullName"
-                                value={user.fullName}
-                                onChange={handleChange}
-                                required
-                            />
-                        </CardFieldset>
-                        <CardFieldset>
-                            <CardInput
-                                placeholder="Password"
-                                name="password"
-                                type="password"
-                                value={user.password}
-                                onChange={handleChange}
-                                required />
-                            <CardIcon className="fa fa-eye" eye small />
-                        </CardFieldset>
-                        <CardFieldset>
-                            <CardInput
-                                placeholder="Photo Url"
-                                name="photoUrl"
-                                type="link"
-                                value={user.photoUrl}
-                                onChange={handleChange}
-                                required />
-                        </CardFieldset>
-
-                        <CardFieldset>
-                            <CardInput
-                                placeholder="Username"
-                                name="username"
-                                type="text"
-                                value={user.username}
-                                onChange={handleChange}
-                                required />
-                        </CardFieldset>
-
-
-
-                        <CardFieldset>
-
-                            <CardOptions>
-                                <CardOptionsItem>
-                                    <CardIcon className="fab fa-google" big />
-                                </CardOptionsItem>
-
-                                <CardOptionsItem>
-                                    <CardIcon className="fab fa-twitter" big />
-                                </CardOptionsItem>
-
-                                <CardOptionsItem>
-                                    <CardIcon className="fab fa-facebook" big />
-                                </CardOptionsItem>
-                            </CardOptions>
-                        </CardFieldset>
-
-                        <CardFieldset>
-                            <CardButton type="submit">Sign Up</CardButton>
-                        </CardFieldset>
-
-                        <CardFieldset>
-                            <CardLink>
-                                <Link to="/UserLogin">I already have an account</Link>
-                            </CardLink>
-                        </CardFieldset>
-                    </CardBody>
-                </CardWrapper>
-            </form>
-        </div>
-    )
-}
+            <CardFieldset>
+              <CardLink>
+                <Link to='/UserLogin'>I already have an account</Link>
+              </CardLink>
+            </CardFieldset>
+          </CardBody>
+        </CardWrapper>
+      </form>
+    </div>
+  );
+};
